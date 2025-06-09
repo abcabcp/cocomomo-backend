8b513694d962e3e16753f63a9a9e7313dbb0812d
seulgi.lee <seulgi.lee@altava.com>
2025-06-09 16:04:49 +0900

    Fix

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from "./app.module";
 import { TransformInterceptor } from "./common/interceptors/transform.interceptor";
 import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   app.useGlobalInterceptors(new TransformInterceptor());
 
@@ -20,6 +22,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(configService.get<number>("PORT") || 3000);
 }
 bootstrap();
